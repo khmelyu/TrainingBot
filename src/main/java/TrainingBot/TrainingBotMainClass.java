076426aa06cae55128e1e9a2667298b7630cf6bd
khmelyu <yurykhmelinin@gmail.com>
2023-07-17 11:59:45 +0300

    Initial commit

---
+++ src/main/java/TrainingBot/TrainingBotMainClass.java
@@ -0,0 +1,12 @@
+package TrainingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ReefCareMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(ReefCareMainClass.class, args);
+    }
+}
