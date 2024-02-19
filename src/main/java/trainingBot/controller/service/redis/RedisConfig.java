e93f16feba77568c69447dc3999c6f62c7a16b55
khmelyu <yurykhmelinin@gmail.com>
2024-02-19 16:02:26 +0300

    test jenkins

--- src/main/java/trainingBot/controller/service/redis/RedisConfig.java
+++ src/main/java/trainingBot/controller/service/redis/RedisConfig.java
@@ -26,9 +26,6 @@ public class RedisConfig {
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
-
-
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
