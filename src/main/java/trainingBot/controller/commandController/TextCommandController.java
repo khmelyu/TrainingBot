5891bc4507c095fe085fee7b36357d1795b7c9d1
khmelyu <yurykhmelinin@gmail.com>
2024-03-26 10:43:14 +0300

    check userstate != null recursion

--- src/main/java/trainingBot/controller/commandController/TextCommandController.java
+++ src/main/java/trainingBot/controller/commandController/TextCommandController.java
@@ -34,63 +34,68 @@ public class TextCommandController implements CommandController {
         Long id = update.getMessage().getChatId();
         String text = update.getMessage().getText();
         UserState userState = userStateService.getUserState(id);
-        if (text.equals("/start")) {
-            startAction.startAction(id);
-        }
-        for (Button button : Button.values()) {
-            if (button.getText().equals(text)) {
-                switch (button) {
-                    case BACK -> backAction.backAction(id);
-                    //MainMenu
-                    case TRAININGS -> mainMenuAction.trainingsAction(id);
-                    case DOCUMENTS -> mainMenuAction.documents(id);
-                    case MY_DATA -> mainMenuAction.userData(id);
-                    case FEEDBACK -> mainMenuAction.feedback(id);
-                    //MyData
-                    case ITS_OK -> mainMenuAction.userDataOk(id);
-                    case CHANGE -> mainMenuAction.userDataFail(id);
-                    //Documents
-                    case WORKNOTE -> documentsAction.worknote(id);
-                    case WORKNOTE_TRAINEE -> documentsAction.traineeNote(id);
-                    case WORKNOTE_SECOND -> documentsAction.secondLevelNote(id);
-                    case WORKNOTE_THIRD -> documentsAction.thirdLevelNote(id);
-                    case WORKNOTE_FOURTH -> documentsAction.fourthLevelNote(id);
-                    case WORKNOTE_FIFTH -> documentsAction.fifthLevelNote(id);
+        if (userState != null) {
+            if (text.equals("/start")) {
+                startAction.startAction(id);
+            }
+            for (Button button : Button.values()) {
+                if (button.getText().equals(text)) {
+                    switch (button) {
+                        case BACK -> backAction.backAction(id);
+                        //MainMenu
+                        case TRAININGS -> mainMenuAction.trainingsAction(id);
+                        case DOCUMENTS -> mainMenuAction.documents(id);
+                        case MY_DATA -> mainMenuAction.userData(id);
+                        case FEEDBACK -> mainMenuAction.feedback(id);
+                        //MyData
+                        case ITS_OK -> mainMenuAction.userDataOk(id);
+                        case CHANGE -> mainMenuAction.userDataFail(id);
+                        //Documents
+                        case WORKNOTE -> documentsAction.worknote(id);
+                        case WORKNOTE_TRAINEE -> documentsAction.traineeNote(id);
+                        case WORKNOTE_SECOND -> documentsAction.secondLevelNote(id);
+                        case WORKNOTE_THIRD -> documentsAction.thirdLevelNote(id);
+                        case WORKNOTE_FOURTH -> documentsAction.fourthLevelNote(id);
+                        case WORKNOTE_FIFTH -> documentsAction.fifthLevelNote(id);
 
-                    case PATTERNS -> documentsAction.patterns(id);
-                    case SALARY -> documentsAction.salary(id);
-                    case STATEMENTS -> documentsAction.statements(id);
-                    case INVENTORY -> documentsAction.inventory(id);
+                        case PATTERNS -> documentsAction.patterns(id);
+                        case SALARY -> documentsAction.salary(id);
+                        case STATEMENTS -> documentsAction.statements(id);
+                        case INVENTORY -> documentsAction.inventory(id);
 
-                    case CERTIFICATES -> documentsAction.certificates(id);
-                    case TEA -> documentsAction.tea(id);
-                    case COFFEE -> documentsAction.coffee(id);
-                    case ACCESSORIES -> documentsAction.accessories(id);
-                    case SWEETS -> documentsAction.sweets(id);
-                    case PACKAGE -> documentsAction.packages(id);
-                    case HOUSEHOLD -> documentsAction.household(id);
-                    case CHOCOSTYLE -> documentsAction.chocostyle(id);
+                        case CERTIFICATES -> documentsAction.certificates(id);
+                        case TEA -> documentsAction.tea(id);
+                        case COFFEE -> documentsAction.coffee(id);
+                        case ACCESSORIES -> documentsAction.accessories(id);
+                        case SWEETS -> documentsAction.sweets(id);
+                        case PACKAGE -> documentsAction.packages(id);
+                        case HOUSEHOLD -> documentsAction.household(id);
+                        case CHOCOSTYLE -> documentsAction.chocostyle(id);
 
-                    case COMPETITIONS -> documentsAction.competitions(id);
-                    case CORPORATE -> documentsAction.corporate(id);
-                    case CONSULTANT -> documentsAction.consultant(id);
-                    case CURATOR -> documentsAction.curator(id);
-                    case MANAGER -> documentsAction.manager(id);
+                        case COMPETITIONS -> documentsAction.competitions(id);
+                        case CORPORATE -> documentsAction.corporate(id);
+                        case CONSULTANT -> documentsAction.consultant(id);
+                        case CURATOR -> documentsAction.curator(id);
+                        case MANAGER -> documentsAction.manager(id);
+                    }
                 }
             }
-        }
-        switch (userState) {
-            case START -> startAction.inputName(update);
+            switch (userState) {
+                case START -> startAction.inputName(update);
 
-            case LOGIN -> startAction.inputLogin(update);
-            case PASSWORD -> startAction.inputPassword(update);
+                case LOGIN -> startAction.inputLogin(update);
+                case PASSWORD -> startAction.inputPassword(update);
 
-            case SET_NAME -> startAction.addName(update);
-            case SET_LASTNAME -> startAction.addLastName(update);
-            case SET_PHONE -> startAction.addPhone(update);
-            case SET_CITY -> startAction.addCity(update);
-            case SET_GALLERY -> startAction.addGallery(update);
-            case SET_RATE -> startAction.addRate(update);
+                case SET_NAME -> startAction.addName(update);
+                case SET_LASTNAME -> startAction.addLastName(update);
+                case SET_PHONE -> startAction.addPhone(update);
+                case SET_CITY -> startAction.addCity(update);
+                case SET_GALLERY -> startAction.addGallery(update);
+                case SET_RATE -> startAction.addRate(update);
+            }
+        } else {
+            userStateService.setUserState(id, UserState.TRAININGS_MENU);
+            handleMessage(update);
         }
     }
 }
