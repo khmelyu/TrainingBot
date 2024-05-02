87b82a03bf569c808a91e0469e3bf7240f83b300
khmelyu <yurykhmelinin@gmail.com>
2024-05-02 13:20:26 +0300

    update czSearch

--- src/main/java/trainingBot/model/rep/CantataZnaetRepository.java
+++ src/main/java/trainingBot/model/rep/CantataZnaetRepository.java
@@ -8,6 +8,6 @@ import trainingBot.model.entity.CantataZnaet;
 import java.util.List;
 
 public interface CantataZnaetRepository extends JpaRepository<CantataZnaet, Long> {
-    @Query("SELECT cz FROM cantata_znaet cz WHERE LOWER(cz.title) LIKE LOWER(:title)")
+    @Query("SELECT cz FROM cantata_znaet cz WHERE LOWER(cz.title) LIKE LOWER(CONCAT('%', REPLACE(:title, ' ', '%'), '%'))")
     List<CantataZnaet> findByTitleLike(@Param("title") String title);
 }
